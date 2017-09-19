f6d9f7eee552b99ecb58f7eac3fe13e774a3acbe
Asd1995sse <Asd1995sse@yandex.ru>
2017-09-19 08:28:10 +0300

    reliaze

--- src/core/Loader.java
+++ src/core/Loader.java
@@ -1,5 +1,28 @@
 package core;
 
 public class Loader {
-
+	 static double disk = 1.44; //мб
+	 static double v3disk = 24; //кубмм
+	 static double mdisk = 4; //граммы	 
+	 public static void main(String []args) {
+		 System.out.println("Equestria System Graphic Lib_v1.0... Init");
+		 calcul();
+		 
+			 
+		 }
+		 
+	 public static Object calcul(){
+		 double fz = 60;
+		 double mbfz = fz*1024;
+		 double xernia = mbfz / disk;
+		 System.out.println(xernia + " - Количество дискет");
+		 double v3all = v3disk*xernia;
+		 System.out.println(v3all + " - Объем дискет");
+		 double massa = xernia*mdisk;
+		 System.out.println(massa + " - Вес дискет");
+		return null;		 
+		 
+		
+	 }	 
+		
 }
